edd150768970b5df39cf1a0494eefda521fdf54d
sandeep <sandyrajsli@gmail.com>
2023-09-09 21:31:42 +0530

    new commit for publich

---
+++ provider.tf
@@ -0,0 +1,17 @@
+# Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+#  access_key = "${var.aws_access_key}"
+#  secret_key = "${var.aws_secret_key}"
+  region  = var.aws_regions
+  profile = "default"
+}
